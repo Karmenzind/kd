d87eb93ba0ad1e85322ce8963cb3eab78d00a34a
qike <qi.ke@ijooz.com>
2025-08-10 16:06:07 +0800

    add musl support for better compatibility; added musl-cross files :eyes:

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,91 +1,118 @@
 #!/usr/bin/env bash
 
-CURDIR=$(
-	cd $(dirname $0)
-	pwd
-)
+CURDIR=$(cd `dirname $0` && pwd)
+
 PROJECT_DIR=$(dirname $CURDIR)
 cd $PROJECT_DIR
 
 targetdir=${PROJECT_DIR}/build
 mkdir -p $targetdir
 
+get_os_type() {
+    local os_type="$(uname -s)"
+    case "$os_type" in
+        Darwin)
+            echo "darwin"
+            ;;
+        Linux)
+            echo "linux"
+            ;;
+        MINGW*|MSYS*|CYGWIN*)
+            echo "windows"
+            ;;
+    esac
+}
+
 do_build() {
-	local os=$1
-	local arch=$2
-	local targetfile=$3
+    local os=$1
+    local arch=$2
+    local targetfile=$3
+    local ldflags="-s -w"
 
-	local cgo=1 cc=
+    [[ -z $os ]] && os=$(get_os_type)
+    [[ -z $arch ]] && arch=$(go env GOARCH)
 
-	if [[ $targetfile == "" ]] && [[ $os != "" ]] && [[ $arch != "" ]]; then
-		echo
-		echo "≫  Building for $os $arch..."
+    local cgo=1 cc=
 
-		local filename=kd_${os}_${arch}
-		[[ $os == "darwin" ]] && filename=kd_macos_${arch}
+    if [[ $targetfile == "" ]] && [[ $os != "" ]] && [[ $arch != "" ]]; then
+        echo
+        echo "≫  Building for $os $arch..."
 
-		local targetfile=${targetdir}/${filename}
-		[[ $os == "windows" ]] && targetfile=${targetfile}.exe
-	fi
+        local filename=kd_${os}_${arch}
+        [[ $os == "darwin" ]] && filename=kd_macos_${arch}
 
-	case $os in
-	windows)
-		local cc=x86_64-w64-mingw32-gcc
-		# local buildopts=-buildmode=c-shared
-		;;
-	linux)
-		if [[ $arch == "arm64" ]]; then
-			local cc=aarch64-linux-gnu-gcc
-		fi
-		;;
-	darwin)
-		# TODO (k): <2023-12-21>
-		;;
-	esac
+        local targetfile=${targetdir}/${filename}
+        [[ $os == "windows" ]] && targetfile=${targetfile}.exe
+    fi
 
-	set -x
-	GOOS=$os GOARCH=$arch CGO_ENABLED=$cgo CC=$cc go build \
+    case $os in
+    windows)
+        cc=x86_64-w64-mingw32-gcc
+        # local buildopts=-buildmode=c-shared
+        ;;
+    linux)
+        if [[ -f /etc/alpine-release ]]; then
+            cc=gcc
+            cc=aarch64-alpine-linux-musl-gcc
+        else
+            if [[ $arch == "arm64" ]]; then
+                cc=aarch64-linux-musl-gcc
+            else
+                cc=musl-gcc
+            fi
+        fi
+        ldflags="-extldflags \"-static\" ${ldflags}" \
+        ;;
+    darwin)
+        # TODO (k): <2023-12-21>
+        ;;
+    esac
+
+    echo "Using: GOOS=$os GOARCH=$arch CGO_ENABLED=$cgo CC=$cc"
+
+    set -x
+    GOOS=$os GOARCH=$arch CGO_ENABLED=$cgo CC=$cc go build \
         ${buildopts} \
         -o ${targetfile} \
-        -ldflags="-s -w" \
+        -ldflags="${ldflags}" \
         -tags urfave_cli_no_docs \
         cmd/kd/kd.go
-	local ret=$?
-	set +x
+    local ret=$?
+    set +x
 
-	if (($ret == 0)); then
-		echo "    [✔] Finished -> ${targetfile}"
-	else
-		echo "    [✘] Failed to build for $os $arch"
-	fi
+    if (($ret == 0)); then
+        echo "    [✔] Finished -> ${targetfile}"
+    else
+        echo "    [✘] Failed to build for $os $arch"
+    fi
 }
 
 build_all() {
-	declare -A d=(
-		["darwin"]="amd64 arm64"
-		["linux"]="amd64 arm64"
-		["windows"]="amd64"
-	)
-	rm -rfv $targetdir/*
-	for os in "${!d[@]}"; do
-		for arch in ${d[$os]}; do
-			do_build $os $arch $targetfile
-		done
-	done
+    declare -A d=(
+        ["darwin"]="amd64 arm64"
+        ["linux"]="amd64 arm64"
+        ["windows"]="amd64"
+    )
+    rm -rfv $targetdir/*
+    for os in "${!d[@]}"; do
+        for arch in ${d[$os]}; do
+            do_build $os $arch $targetfile
+        done
+    done
 }
 
 case $1 in
 "")
-	echo ">>> Building for current workspace..."
-	# do_build '' '' ${PROJECT_DIR}/kd
-	# do_build '' '' /usr/bin/kd
-	do_build '' '' /usr/local/bin/kd
-	exit
-	;;
+    echo ">>> Building for current workspace..."
+    # do_build '' '' ${PROJECT_DIR}/kd
+    # do_build '' '' /usr/bin/kd
+    do_build '' '' /usr/local/bin/kd
+    exit
+    ;;
 -a | --all)
-	build_all
-	;;
+    build_all
+    ;;
 *)
-	do_build $*
-	;;
+    do_build $*
+    ;;
 esac
